34a982b3bd7ad3e82adea985e6434bed6f210749
xiaowen <liuxiaowen@baimaohui.net>
2023-03-28 16:33:52 +0800

    init

---
+++ lua/plugins.lua
@@ -0,0 +1,21 @@
+-- return require('packer').startup(function()
+--   -- Packer can manage itself
+--   use 'wbthomason/packer.nvim'
+-- end)
+
+
+
+-- return require('packer').startup(function()
+--     -- Packer can manage itself
+--     use 'wbthomason/packer.nvim'
+--     -- gruvbox theme
+--     use {
+--         "ellisonleao/gruvbox.nvim",
+--         requires = {"rktjmp/lush.nvim"}
+--     }
+--     -- nvim-tree (新增)
+--     use {
+--         'kyazdani42/nvim-tree.lua',
+--         requires = 'kyazdani42/nvim-web-devicons'
+--     }
+-- end)
