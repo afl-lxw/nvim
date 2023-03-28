1a6cf87d247c96bf427ca4352341ee7634cecb2b
xiaowen <liuxiaowen@baimaohui.net>
2023-03-28 17:43:19 +0800

    feat: lua update

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 ## nvim 配置
+
+
+### Packer.nvim
+
+```lua
+-- Regenerate compiled loader file
+:PackerCompile
+
+-- Remove any disabled or unused plugins
+:PackerClean
+
+-- Clean, then install missing plugins
+:PackerInstall
+
+-- Clean, then update and install plugins
+:PackerUpdate
+
+-- Perform `PackerUpdate` and then `PackerCompile`
+:PackerSync
+
+-- Loads opt plugin immediately
+:PackerLoad completion-nvim ale
+```
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,21 +1,25 @@
--- return require('packer').startup(function()
---   -- Packer can manage itself
---   use 'wbthomason/packer.nvim'
--- end)
 
 
 
--- return require('packer').startup(function()
---     -- Packer can manage itself
---     use 'wbthomason/packer.nvim'
---     -- gruvbox theme
---     use {
---         "ellisonleao/gruvbox.nvim",
---         requires = {"rktjmp/lush.nvim"}
---     }
---     -- nvim-tree (新增)
---     use {
---         'kyazdani42/nvim-tree.lua',
---         requires = 'kyazdani42/nvim-web-devicons'
---     }
--- end)
+
+return require('packer').startup(function()
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    -- gruvbox theme
+    use {
+        "ellisonleao/gruvbox.nvim",
+        requires = {"rktjmp/lush.nvim"}
+    }
+    -- zephyr
+    use 'glepnir/zephyr-nvim'
+    --  nord
+    use 'shaunsingh/nord.nvim'
+    -- nvim-tree 
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = 'kyazdani42/nvim-web-devicons'
+    }
+    -- treesitter 
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+end)
